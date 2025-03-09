aa032ace1781f1bcbb062b37494cbdf6825c30ba
Avi0395 <avidipakpatil.com>
2025-03-10 00:04:17 +0530

    port related changes

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(express.json());
 const schoolRoutes = require("./routes/schoolRoutes");
 app.use("/api/schools", schoolRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
